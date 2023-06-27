83a6b9ab2c6fa24a682afff9842e844fbb93cc0e
yurcik45 <yurcik45mail@gmail.com>
2023-06-28 00:15:07 +0300

    implement main db methods use class

--- users_db/db_init.rb
+++ users_db/db_init.rb
@@ -8,13 +8,41 @@ $DBNAME = ENV['DBNAME']
 $USER = ENV['USER']
 $PASSWORD = ENV['PASSWORD']
 
-def get_users
-  # Establish the connection
-  conn = PG::Connection.new(host: $HOST, port: $PORT, dbname: $DBNAME, user: $USER, password: $PASSWORD)
+class Users
+  def initialize
+    @users = Array.new
+    @conn = Users.connect_to_db
+  end
 
-  # Execute a SQL query
-  result = conn.exec('SELECT * FROM users')
+  def self.connect_to_db
+    return PG::Connection.new(host: $HOST, port: $PORT, dbname: $DBNAME, user: $USER, password: $PASSWORD)
+  end
 
-  conn.close
-  return result
+  def get_all_users
+    @users = @conn.exec("SELECT * FROM users")
+    # @conn.close
+    return @users
+  end
+
+  def get_user(id)
+    user = @conn.exec("SELECT * from users where id=#{id}")
+    return user
+  end
+
+  def add_user(name, age)
+    @conn.exec("INSERT INTO users (name, age) VALUES ('#{name}', #{age})")
+  end
+
+  def delete_user(id)
+    @conn.exec("DELETE FROM users WHERE id=#{id}")
+  end
+  
+  def change_age(id, age)
+    @conn.exec("UPDATE users SET age=#{age} WHERE id=#{id}")
+  end
+  
+  def change_name(id, name)
+    @conn.exec("UPDATE users SET name='#{name}' WHERE id=#{id}")
+  end
+
 end
--- users_db/main.rb
+++ users_db/main.rb
@@ -1,10 +1,38 @@
 require './db_init.rb'
 
-users = get_users
+$users_one = Users.new
 
-# Process the query result
-users.each do |row|
-  # Access row data
-  puts row['name']
-  puts row['age']
+def show_users
+  puts "========= all users"
+  $users_one.get_all_users.each do |users|
+    puts "id: #{users['id']}"
+    puts "name: #{users['name']}"
+    puts "age: #{users['age']}"
+    puts "--------"
+  end
 end
+
+# all users
+show_users
+puts "========= user with id 1"
+puts $users_one.get_user(1)[0]
+puts "========= add user with name Alex and age 30"
+$users_one.add_user("Alex", 30)
+show_users
+puts "========= get all users and find name Alex then get id"
+$alex_id
+$users_one.get_all_users.each do |users|
+  if users['name'] === "Alex"
+    $alex_id = users['id']
+  end
+end
+puts "Alex id is: #{$alex_id}"
+puts "change age to 35 in user with id #{$alex_id}"
+$users_one.change_age($alex_id, 35)
+show_users
+puts "change name to Alexxx in user with id #{$alex_id}"
+$users_one.change_name($alex_id, "Alexxx")
+show_users
+puts "delete user with id #{$alex_id}"
+$users_one.delete_user($alex_id)
+show_users
