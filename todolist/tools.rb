6dcabd68ff48be0644dee65594892264f5739d82
yurcik45 <yurcik45mail@gmail.com>
2023-06-29 22:37:15 +0300

    add functions to check auth token and halt in error -- not tested

--- todolist/tools.rb
+++ todolist/tools.rb
@@ -29,6 +29,30 @@ def send_wrong_msg(message = "something went wrong")
   body msg.to_json
 end
 
+def send_auth_error_msg
+  status 401
+  msg = { message: "Authorization error" }
+  msg.to_h
+  body msg.to_json
+end
+
+def check_auth_token(token)
+  return false if !token
+  users_db = Users.new
+  init_user_data = decode_token(token)
+  email = init_user_data[:email]
+  user_in_db = users_db.get_user_by_email(email)
+  return false if user_in_db.ntuples === 0
+  db_user_data = user_in_db[0]
+  test_token = generate_token(db_user_data)
+  return true if token === test_token
+  return false
+end
+
+def check_auth_and_halt(request)
+  halt 401, send_auth_error_msg if !check_auth(request.env['HTTP_AUTHORIZATION'])
+end
+
 $jwt_key = ENV['JWT_KEY']
 
 def generate_token(payload)
