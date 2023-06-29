fe9c782a9297f6db7256a76000dd037a8de4896e
yurcik45 <yurcik45mail@gmail.com>
2023-06-29 12:13:56 +0300

    add map types to boolean for postgresql

--- todolist/db.rb
+++ todolist/db.rb
@@ -13,6 +13,10 @@ $table = "todolist"
 class Tasks
   def initialize
     @conn = Tasks.connect_to_db
+    # Register a custom type caster for the boolean column
+    @conn.type_map_for_results = PG::BasicTypeMapForResults.new(@conn).tap do |tm|
+      tm.add_coder(PG::TextDecoder::Boolean.new)
+    end
   end
 
   def self.connect_to_db
@@ -24,11 +28,11 @@ class Tasks
   end
 
   def get_task(id)
-    return @conn.exec("SELECT * from #{$table} where id=#{id}")
+    return @conn.exec("SELECT * FROM #{$table} WHERE id=#{id}")
   end
 
   def add_task(task)
-    return @conn.exec("INSERT INTO #{$table} (task, completed) VALUES ('#{task}', #{false}) RETURNING *")
+    return @conn.exec("INSERT INTO #{$table} (task) VALUES ('#{task}') RETURNING *")
   end
     
   def change_task(id, task, completed)
